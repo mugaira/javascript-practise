784407e4e81b0af8e1a903d0f2dd833820a5aaec
mugaira <mugairainamdar@gmail.com>
2022-07-25 22:47:43 +0530

    promise methods

--- promise_request.js
+++ promise_request.js
@@ -218,3 +218,149 @@
 //  })
 //  .catch((err) => console.log(err));
 
+//-----XMLHttpRequest----------
+
+// const req = new XMLHttpRequest() // make new object
+// // provide 2 callbacks
+// req.onload = function () { // if successful
+// const data = JSON.parse(this.responseText)
+// console.log(data)
+// }
+// req.onerror = function (err) { // if failure
+// console.log('ERROR', err)
+// }
+// req.open('get', 'https://swapi.dev/api/people/1',true) // request type and url
+// req.setRequestHeader('Accept', 'application/json')// send headers
+// req.send() // send request
+
+// const req = new XMLHttpRequest();
+// // Adding event listeners instead of attributes
+// req.addEventListener("load", function () {
+//  const data = JSON.parse(this.responseText);
+//  // console.log('It worked!', data)
+//  for (let planet of data.results) {
+//   console.log(planet);
+//   console.log(planet.name);
+//   console.log(planet.diameter);
+//  }
+// });
+// req.addEventListener("error", function (err) {
+//  console.log("Error", err);
+// });
+// req.open("get", "https://swapi.dev/api/planets", true);
+// req.send();
+// console.log(req); // req object has the responseText field;
+
+// const req = new XMLHttpRequest();
+// req.addEventListener("load", function () {
+//   console.log("First Request");
+//   const data = JSON.parse(this.responseText);
+//   // Second Request (nested)
+//   const filmUrl = data.results[0].films[0];
+//   const filmReq = new XMLHttpRequest();
+//   filmReq.addEventListener("load", function () {
+//     console.log("Second Request");
+//     const filmData = JSON.parse(this.responseText);
+//     console.log(filmData.title);
+//   });
+//   filmReq.addEventListener("error", function (err) {
+//     console.log(err);
+//   });
+//   filmReq.open("GET", filmUrl);
+//   filmReq.send();
+// });
+// req.addEventListener("error", function (err) {
+//   console.log("Error", err);
+// });
+// req.open("get", "https://swapi.dev/api/planets", true);
+// req.send();
+
+//-------------Fetch---------------------
+
+// fetch("https://swapi.dev/api/planets", {
+//  headers: { Accept: "application/json" },
+// })
+//  .then((response) => {
+//   if (response.status !== 200) {
+//    console.log("Problem", response.status);
+//    return;
+//   }
+//   // Have to use response.json as we get back a stream;
+//   // and not the json directly.
+//   response.json().then((data) => {
+//    // itself is a promise;
+//    console.log(data);
+//   });
+//  })
+//  .catch(function (err) {
+//   console.log("Fetch err", err);
+//  });
+
+// fetch("https://swapi.dev/api/planets")
+//  .then((response) => {
+//   if (!response.ok) {
+//    throw new Error(`Status code error: ${response.status}`);
+//   }
+//   return response.json();
+//  })
+//  .then((data) => {
+//   console.log("Fetched all planets");
+//   console.log(data.results[0].films[0]);
+//   const filmUrl = data.results[0].films[0];
+
+//   return fetch(filmUrl);
+//  })
+//  .then((response) => {
+//   if (!response.ok) {
+//    throw new Error(`Status code error:${response.status}`);
+//   }
+//   return response.json();
+//  })
+//  .then((data) => {
+//   console.log("Fetched first film");
+//   console.log(data.title);
+//  })
+//  .catch((err) => {
+//   console.log("ERROR!", err);
+//  });
+
+//--------------Axios--------------------
+
+// axios
+//  .get('https://swapi.dev/api/planets')
+//  .then(data => {
+//   // data is already parsed for us
+//   const filmUrl = data.data.results[0].films[0]
+//   console.log(data.data.results[0].films[0]);
+//   return axios.get(filmUrl)
+//  })
+//  .then(data => {
+//   console.log(data.data.title)
+//  })
+//  .catch(err => console.log('In Catch Block: \n\n',err))
+// // You don't have to parse JSON
+// You don't have to manually segregate the bad status codes
+
+// axios
+//  .get("https://swapi.dev/api/planets")
+//  .then(({ data }) => {
+//   for (let planet of data.results) {
+//    console.log(planet.name);
+//   }
+//   return axios.get(data.next);
+//  })
+//  .then(({ data }) => {
+//   for (let planet of data.results) {
+//    console.log(planet.name);
+//   }
+//   return axios.get(data.next);
+//  })
+//  .then(({ data }) => {
+//   for (let planet of data.results) {
+//    console.log(planet.diameter);
+//   }
+//  })
+//  .catch((err) => console.log("ERROR OCCURRED", err));
+
+
+
